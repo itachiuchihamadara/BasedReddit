43697599f676d2c9713123e193081d9b256614f6
itachiuchihamadara <sgrganesh1@gmail.com>
2024-08-25 17:13:32 +0530

    removing useless imports

--- src/main/java/com/based/genzedong/BasedReddit/BasedRedditApplication.java
+++ src/main/java/com/based/genzedong/BasedReddit/BasedRedditApplication.java
@@ -1,27 +1,14 @@
  package com.based.genzedong.BasedReddit;
 
-import com.based.genzedong.BasedReddit.services.PostServices;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.print.DocFlavor;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
+ import org.springframework.boot.SpringApplication;
+ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
  @SpringBootApplication
 public class BasedRedditApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(BasedRedditApplication.class, args);
 
-		List<Integer> st = new ArrayList<>();
-		st.add(10); st.add(20);
-
-		int i = st.stream().reduce(0, Integer::sum);
-		System.out.println(i);
+		SpringApplication.run(BasedRedditApplication.class, args);
 
 	}
 
